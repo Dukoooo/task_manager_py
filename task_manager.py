e0a058c3599a7936d33321f06de59477d071334f
Juraj Golian <118574181+Dukoooo@users.noreply.github.com>
2025-11-19 22:47:18 +0100

    feat: implemented addTask, showTasks and closeMenu functions

--- task_manager.py
+++ task_manager.py
@@ -1,11 +1,34 @@
+tasks = []
+
 def pridat_ulohu():
-    print("Funkcia pridat_ulohu() bola spustena.")
+    
+    while True:
+        task_name = input("Zadajte názov úlohy: " )
+        task_des = input("Zadajte popis úlohy: " )
+
+        if task_name != "" and task_des != "":
+            tasks.append({"name": task_name, "description": task_des})
+        else:
+            print("Zadali ste neplatné údaje...")
+        break
 
 def zobrazit_ulohy():
-    print("Funkcia zobrazit_ulohy() bola spustena.")
+    count = 0
+    if tasks != []:
+        for task in tasks:
+            count += 1
+            print("Zoznam úloh: ")
+            print(f"{count} {task['name']} - {task['description']}")
+    else:
+        print("Žiadne tasky k dispozícii...")
+
 
 def odstranit_ulohu():
     print("Funkcia odstranit_ulohu() bola spustena.")
+
+
+def koniec_programu():
+    hlavne_menu.destroy()
 
 def hlavne_menu():
     while True:
@@ -14,16 +37,16 @@ def hlavne_menu():
         print("2. Zobrazit vsetky ulohy")
         print("3. Odstranit ulohu")
         print("4. Koniec programu")
+        option = input("Vyberte moznost (1-4): ")
+        print("\n")
 
-        volba = input("Vyberte moznost (1-4): ")
-
-        if volba == '1':
+        if option == '1':
             pridat_ulohu()
-        elif volba == '2':
+        elif option == '2':
             zobrazit_ulohy()
-        elif volba == '3':
+        elif option == '3':
             odstranit_ulohu()
-        elif volba == '4':
+        elif option == '4':
             print("Program konci, ahoj!")
             break
         else:
